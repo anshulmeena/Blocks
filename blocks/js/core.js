cadf5dfdad10b8797e842f7a807e6ae11c74ecc4
Nathan Curtis <nathan@eightshapes.com>
2012-02-25 13:57:50 -0500

    Updated License to GPL

--- blocks/js/core.js
+++ blocks/js/core.js
@@ -1,9 +1,22 @@
 /*!
- * EightShapes Blocks framework 2012.02.07
+ * EightShapes Blocks framework
  * http://eightshapes.com
  *
- * Copyright 2012, EightShapes LLC
+ * ©Copyright 2012 Eight Shapes LLC
+ *
  * Terms of Use: http://unify.eightshapes.com/about-the-system/terms-of-use/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 var EightShapes = EightShapes || {};
